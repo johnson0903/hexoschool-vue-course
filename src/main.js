8aa6ba94d5160308e51de12e88791c102133de70
johnson0903 <johnson50701@gmail.com>
2019-07-27 11:21:55 +0800

    meta requireAuth axios credentials

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios)
+
+axios.defaults.withCredentials = true;
 
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app');
+
+router.beforeEach((to, from, next) => {
+
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`;
+    axios.post(api).then(response => {
+      console.log(response.data);
+      if (response.data.success) {
+        next()
+      } else {
+        next('login')
+      }
+    });
+  } else {
+    console.log('不需要驗證')
+    next()
+  }
+
+})
