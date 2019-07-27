9a6e513141c25e0bed8ff5e69b151a6763d06a49
johnson0903 <johnson50701@gmail.com>
2019-07-27 11:37:41 +0800

    add login page, dashboard page import bootstrap, variable scss

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
 import Vue from 'vue';
-import App from './App.vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import 'bootstrap';
+
+import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
 
 axios.defaults.withCredentials = true;
 
 new Vue({
   router,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app');
 
 router.beforeEach((to, from, next) => {
-
   if (to.meta.requiresAuth) {
     const api = `${process.env.VUE_APP_APIPATH}/api/user/check`;
     axios.post(api).then(response => {
       console.log(response.data);
       if (response.data.success) {
-        next()
+        next();
       } else {
-        next('login')
+        next('login');
       }
     });
   } else {
-    console.log('不需要驗證')
-    next()
+    console.log('不需要驗證');
+    next();
   }
-
-})
+});
--- src/router.js
+++ src/router.js
@@ -1,23 +1,40 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Login from './components/pages/Login.vue'
+import Dashboard from './components/Dashboard.vue'
+import Products from './components/pages/Products.vue'
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
+  mode: 'history',
+  routes: [{
+      path: '*',
+      redirect: '/login'
+    },
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '/login',
+      name: 'Login',
+      component: Login
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      name: 'About',
+      component: About
     },
+    {
+      path: '/admin',
+      name: 'dashboard',
+      component: Dashboard,
+      children: [{
+        path: 'products',
+        name: 'Products',
+        component: Products,
+        meta: {
+          requiresAuth: true
+        },
+      }]
+    }
   ],
 });
